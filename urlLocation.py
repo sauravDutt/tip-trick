fb533e978db162ee44016d91e4031813c085f2b7
sauravDutt <sauravd069@gmail.com>
2021-06-21 21:23:26 +0530

    Just added my first TipAndTrick

---
+++ urlLocation.py
@@ -0,0 +1,12 @@
+# get IP and loacation from a url
+
+import socket
+from ip2geotools.databases.noncommercial import DbIpCity
+
+url = input("Insert a URL: ")
+ip = socket.gethostbyname(url)
+response = DbIpCity.get(ip, api_key='free')
+print("IP: ", ip)
+print("City: ", response.city)
+print("Region: ", response.region)
+print("Country: ", response.country)
